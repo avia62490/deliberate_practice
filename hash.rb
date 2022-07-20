3d3fd21ee4ee262eab73252dc96e5dd0ff93fea2
Alex Via <alexandervia@gmail.com>
2022-07-19 21:36:25 -0500

    completed some hash exercises

--- hash.rb
+++ hash.rb
@@ -1,20 +1,77 @@
 # 1. Make a hash to store a person's first name, last name, and email address. 
 #Then print each attribute on separate lines.
 
+# person1 = {
+#   "first_name" => "John",
+#   "last_name" => "Ranger",
+#   "email" => "johnranger@gmail.com"
+# }
+
+# puts person1["first_name"]
+# puts person1["last_name"]
+# puts person1["email"]
+
 # 2. Make an array of hashes to store the first name and last name for 3 different people.
 #Then print out the first person's info.
+
+# people = [
+#   {:first_name => "Finn", :last_name => "Mertens", :email => "fmertens@gmail.com"},
+#   {:first_name => "Walter", :last_name => "White", :email => "Heisenberg@gmail.com"},
+#   {:first_name => "Seymour", :last_name => "Skinner", :email => "Atamzarian@gmail.com"}
+# ]
+# p people[0][:first_name]
+# p people[0][:last_name]
+# p people[0][:email]
 
 # 3. Make a hash to store prices for 3 different menu items. 
 #Then add a new menu item and price and print the hash to see the result.
 
+# menu = {
+#   :hamburger => 7,
+#   :nuggets => 5,
+#   :fries => 3
+# }
+# p menu
+# menu[:shake] = 4
+# p menu
+
 # 4. Make a hash to store a book's title, author, number of pages, and language.
 #Then print each attribute on separate lines.
+
+# book = {
+#   :title => "How to Change Your Mind",
+#   :author => "Michael Pollan" ,
+#   :pages => 350 , 
+#   :language => "English"
+# }
+
+# p book[:title]
+# p book[:author]
+# p book[:pages]
+# p book[:language]
+
 
 # 5. Make an array of hashes to store the title and author for 3 different books.
 #Then print out the third book's author.
 
+# books = [
+#   {:title => "Sapiens", :author => "Yuval Noah Harari"},
+#   {:title => "The Cat in the Hat", :author => "Dr. Seuss"},
+#   {:title => "The Iliad", :author => "Homer"}
+# ]
+# p books[2][:author]
+
 # 6. Make a hash to store 3 different states and their captitals.
 #Then add a new state and capital and print the hash to see the result.
+
+# capital_cities = {
+#   :Illinois => "Springfield",
+#   :Minnesota => "St. Paul",
+#   :New_York => "Albany"
+# }
+# p capital_cities
+# capital_cities[:Hawaii] = "Honolulu"
+# p capital_cities
 
 # 7. Make a hash to store a laptop's brand, model, and year.
 #Then print each attribute on separate lines.
