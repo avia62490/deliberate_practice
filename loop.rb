cf413995026c5cddc19c3314f83b2ede17b07470
Alex Via <alexandervia@gmail.com>
2022-07-20 19:33:24 -0500

    Worked on loop.rb exercises

--- loop.rb
+++ loop.rb
@@ -1,20 +1,60 @@
 # 1. Write a while loop to print the numbers 1 through 10.
 
+# number = 1
+# while number < 11
+#   p number
+#   number += 1
+# end
+
 # 2. Write a while loop that prints the word "hello" 5 times.
+
+# 5.times do
+#   p "hello"
+# end
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
+# while true
+#   puts "enter a word"
+#   word = gets.chomp
+#   if word == "stop"
+#     break
+#   end
+# end
+
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# number = 0
+# while number < 101
+#   p number
+#   number += 5
+# end
 
 # 5. Write a while loop that prints the number 9000 ten times.
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# while true
+#   puts "enter a number"
+#   number = gets.chomp.to_i
+#   if number > 10
+#     break
+#   end
+# end
 
 # 7. Write a while loop that prints the numbers 50 to 70.
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# while true
+#   puts "enter a word"
+#   word = gets.chomp
+#   if word.length > 5
+#     break
+#   end
+# end
 
 # 10. Write a while loop that prints the even numbers from 2 to 40.
 
