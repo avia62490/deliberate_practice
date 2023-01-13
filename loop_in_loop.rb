0cd20833139a3699cee08d59cc2cdd06fd02f54f
Alex Via <alexandervia@gmail.com>
2023-01-13 15:50:43 -0600

    completes 6-10

--- loop_in_loop.rb
+++ loop_in_loop.rb
@@ -74,17 +74,77 @@
 #  6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
 #     For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
 
+# numbers1 = [1, 2]
+# numbers2 = [6, 7, 8]
+# sums = []
+# numbers1.each do |num1|
+#   numbers2.each do |num2|
+#     sums << num1 + num2
+#   end
+# end
+# p sums
+
 #  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
 #     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
+
+# numbers = [2, 8, 3]
+# products = []
+# numbers.each do |number|
+#   numbers.each do |num|
+#     products << number * num
+#   end
+# end
+# p products
 
 #  8. Use a nested loop to find the largest sum of any two different numbers within an array.
 #     For example, [1, 8, 3, 10] becomes 18.
 
+# numbers = [1, 8, 3, 10]
+# largest_sum = numbers[0] + numbers[1]
+# i = 0
+# while i < numbers.length
+#   j = 0
+#   while j < numbers.length
+#     if i != j && numbers[i] + numbers[j] > largest_sum
+#       largest_sum = numbers[i] + numbers[j]
+#     end
+#     j += 1
+#   end
+#   i += 1
+# end
+# p largest_sum
+
 #  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 #     For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
 
+# numbers = [2, 5, 3, 1, 0, 7, 11]
+# result = false
+# numbers.each do |num1|
+#   numbers.each do |num2|
+#     if num1 != num2 && num1 + num2 == 10
+#       result = [num1, num2]
+#       break
+#     end
+#   end
+#   if result != false
+#     break
+#   end
+# end
+# p result
+
 # 10. Use a nested loop to convert an array of string arrays into a single string.
 #     For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
+
+nested = [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]]
+string = ""
+nested.each do |nest|
+  nest.each do |word|
+    string += word
+  end
+end
+p string
+
+
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/af8985dc4fb07ea14b4bd6ba41cb08f8
