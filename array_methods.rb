ae2a468960a493d308aa09b0cde172d5b25bfdb3
Alex Via <alexandervia@gmail.com>
2025-02-17 12:24:59 -0600

    practices array methods

---
+++ array_methods.rb
@@ -0,0 +1,77 @@
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+# words = ["computer", "keyboard", "mouse"]
+# p words
+# words.push("screen", "speaker")
+# p words
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+# symbols = ["a", "l", "e", "x"]
+# p symbols
+# symbols[1] = 1
+# p symbols
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+# numbers = [1, 1, 2, 3, 5]
+# i = 0
+# while i < numbers.length
+#   puts numbers[i]
+#   i += 1
+# end
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+# numbers = [5]
+# p numbers
+# numbers.push(10, 15, 20)
+# p numbers
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+# strings = ["apple", "banana", "orange"]
+# p strings
+# strings[2] = strings[2].upcase
+# p strings
+
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+# names = ["alex", "brendan", "casey"]
+# i = 0
+# while i < names.length
+#   puts names[i]
+#   i += 1
+# end
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+# strings = ["coffee", "phone"]
+# p strings
+# strings << "backpack"
+# p strings
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+# numbers = [5, 8, 13, 21, 34]
+# p numbers
+# numbers[0] *= 10
+# p numbers
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+# numbers = [23, 69]
+# i = 0
+# while i < numbers.length
+#   puts numbers[i]
+#   i += 1
+# end
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+countries = ["America", "Russia", "China"]
+p countries
+countries.push("Israel")
+p countries
+
+# SOLUTIONS: https:ist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
