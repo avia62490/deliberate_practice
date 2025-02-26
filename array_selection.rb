f873ef51dcd76911c96c919a0d1ceaa55d4a37f5
Alex Via <alexandervia@gmail.com>
2025-02-26 12:10:24 -0600

    practices array selection

--- array_selection.rb
+++ array_selection.rb
@@ -41,18 +41,42 @@
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
+# catalog = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+# short_words = catalog.select {|item| item[:name].length < 6}
+# p catalog
+# p short_words
+
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+# numbers = [8, 23, 0, 44, 1980, 3]
+# small_numbers = numbers.select {|num| num < 10}
+# p numbers
+# p small_numbers
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
+# words = ["big", "little", "good", "bad"]
+# non_bs = words.select {|word| word[0] != "b"}
+# p words
+# p non_bs
+
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+# catalog = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+# cheaper = catalog.select {|item| item[:price] < 10}
+# p catalog
+# p cheaper
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
 
+numbers = [2, 4, 5, 1, 8, 9, 7]
+odds = numbers.select {|num| num.odd?}
+p numbers
+p odds
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/7de16ed43ea506e98df3fa15074b84f8
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/a702894841c7018ed8c127b647ae21f8
