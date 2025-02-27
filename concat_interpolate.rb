867c1c564191a9abebfb42845ea650696ca75b1f
Alex Via <alexandervia@gmail.com>
2025-02-27 13:04:28 -0600

    practices string methods

--- concat_interpolate.rb
+++ concat_interpolate.rb
@@ -29,20 +29,48 @@
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
-color1 = "red"
-color2 = "yellow"
-color3 = "blue"
-puts "the 3 primary colors are #{color1}, #{color2}, and #{color3}."
+# color1 = "red"
+# color2 = "yellow"
+# color3 = "blue"
+# puts "the 3 primary colors are #{color1}, #{color2}, and #{color3}."
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
+# puts "Enter a name: "
+# name = gets.chomp
+# if name == "Santa"
+#   puts "I've been a bad boy ;)"
+# else
+#   puts "tell santa im baddd"
+# end
+
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+# title = "Peace in Every Step"
+# author = "Tich Nat Hanh"
+# puts "I'm reading a book titled '" + title + "' written by " + author
 
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
+# title = "The cat in the hat"
+# author = "Dr. Seuss"
+# puts "I like the book '#{title}' written by #{author}."
+
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# puts "enter the password: "
+# password = gets.chomp
+# if password == "Joshua"
+#   puts "shall we play a game?"
+# else
+#   puts "Access denied!"
+# end
 
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
 
+city1 = "Chicago"
+city2 = "London"
+city3 = "Tokyo"
+puts "they've been on tour in lots of cities around the world including " + city1 + ", " + city2 + ", and " + city3
 
 # SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
