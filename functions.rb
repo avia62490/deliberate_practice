a23a55ca7f30549eff8b6123d5020656faaa4aee
Alex Via <alexandervia@gmail.com>
2025-03-12 12:00:50 -0500

    practices functions

--- functions.rb
+++ functions.rb
@@ -32,21 +32,51 @@
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
-def first_letter(string)
-  return string[0]
-end
-p first_letter("hello")
-p first_letter("world")
+# def first_letter(string)
+#   return string[0]
+# end
+# p first_letter("hello")
+# p first_letter("world")
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+# def combine_strings(string1, string2, string3)
+#   return "#{string1} #{string2} #{string3}"
+# end
+# p combine_strings("see", "spot", "run")
+# p combine_strings("red", "yellow", "blue")
+
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+# def stringify(number)
+#   return number.to_s
+# end
+# p 123
+# p stringify(123)
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
+# def repeat(string)
+#   return string * 5
+# end
+# p repeat("hello")
+# p repeat("echo")
+
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+# def average(num1, num2, num3)
+#   return (num1 + num2 + num3).to_f / 3
+# end
+# p average(12, 18, 33)
+# p average(3, 3, 4)
+
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def mathematics(number)
+  return number * 10 + 30
+end
+p mathematics(2)
+p mathematics(10)
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
