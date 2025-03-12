3c149b21b558369e1d3083555286a824092f4e48
Alex Via <alexandervia@gmail.com>
2025-03-12 11:46:25 -0500

    practices functions

---
+++ functions.rb
@@ -0,0 +1,52 @@
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+# def times_two(number)
+#   return number * 2
+# end
+# p times_two(4)
+# p times_two(7)
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+# def all_caps(string)
+#   return string.upcase
+# end
+# p all_caps("hello world")
+# p all_caps("serenity now!")
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+# def subtraction(num1, num2)
+#   return num1 - num2
+# end
+# p subtraction(12, 5)
+# p subtraction(23, 32)
+
+# 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+# def square(number)
+#   return number ** 2
+# end
+# p square(5)
+# p square(8)
+
+# 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+def first_letter(string)
+  return string[0]
+end
+p first_letter("hello")
+p first_letter("world")
+
+# 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+# 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+# 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+# 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+# 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
